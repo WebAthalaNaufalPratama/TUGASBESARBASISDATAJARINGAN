0d4e7bc8c061105dab4ecb4efb1fef23efa0ba28
firanurhaliza <75962903+firanurhaliza@users.noreply.github.com>
2023-07-06 20:00:54 +0700

    Upload form login, kamar, dan pegawai

---
+++ Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports BCrypt.Net.BCrypt
+Imports System.Data.SqlClient
+Imports System.IO
+
+Public Class Form1
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Dim jam As DateTime = DateTime.Parse("2023-06-30 09:39:23")
+        'MsgBox(jam.ToString())
+        'Me.Hide()
+        'Dim form2 As New Form2()
+        'Form2.ShowDialog()
+        'login
+        passwordBox.UseSystemPasswordChar = True
+        Dim PanjangWindow = Me.ClientSize.Width
+        Dim TinggiWindow = Me.ClientSize.Height
+        'TextBox1.Left = (Me.ClientSize.Width - TextBox1.Width) \ 2
+        'FormName.Location = New Point()
+        'MsgBox(PanjangWindow / 2)
+        Panel.Width = PanjangWindow - 700
+        Panel.Height = TinggiWindow - 300
+        Dim WidthPanel = Panel.Width
+        Dim HeightPanel = Panel.Height
+        Panel.Location = New Point((PanjangWindow - Panel.Width) \ 2, (TinggiWindow - Panel.Height) \ 2)
+        FormName.Location = New Point((WidthPanel - FormName.Width) \ 2, (HeightPanel - FormName.Height) \ 250)
+        'boxUsername.Enabled = False
+        username.Location = New Point((WidthPanel - username.Width) \ 2, (HeightPanel - username.Height) \ 2 - 50)
+        password.Location = New Point((WidthPanel - password.Width) \ 2, (HeightPanel - password.Height) \ 2 + 20)
+        ButtonLogin.Location = New Point((WidthPanel - ButtonLogin.Width) \ 2, (HeightPanel - ButtonLogin.Height) \ 2 + 90)
+    End Sub
+
+    Private Sub ButtonLogin_Click(sender As Object, e As EventArgs) Handles ButtonLogin.Click
+        If boxUsername.Text = "" And passwordBox.Text = "" Then
+            MsgBox("Username dan Password harus diisi !")
+        End If
+        If boxUsername.Text <> "" And passwordBox.Text <> "" Then
+            Dim username = boxUsername.Text
+            Dim password = passwordBox.Text
+            Try
+                Dim connectionString As String = "Data Source=localhost;Initial Catalog=db_hotel;User ID=mine;Password=user@123;"
+                Dim connection As New SqlConnection(connectionString)
+                connection.Open()
+                Dim query As String = "SELECT TOP (1) * FROM [tb_pegawai] WHERE [username] = @value"
+                Dim command As New SqlCommand(query, connection)
+                command.Parameters.AddWithValue("@value", username)
+                Dim reader As SqlDataReader = command.ExecuteReader()
+                Dim path As String = "D:\logs.txt"
+                If reader.Read() Then
+                    Dim resId As Integer = reader.GetInt32(0)
+                    Dim resName As String = reader.GetString(1)
+                    Dim resUsername As String = reader.GetString(2)
+                    Dim resEmail As String = reader.GetString(3)
+                    Dim resPassword As String = reader.GetString(4)
+                    Dim resLevel As String = reader.GetString(5)
+                    My.Computer.FileSystem.WriteAllText(path, reader.Read().ToString(), True)
+                    'MsgBox(resPassword)
+                    If BCrypt.Net.BCrypt.Verify(password, resPassword) Then
+                        User.Id = resId
+                        User.name = resName
+                        Level = resLevel
+                        User.username = resUsername
+                        User.email = resEmail
+                        connection.Close()
+                        Me.Hide()
+                        Dim form2 As New Form2()
+                        form2.ShowDialog()
+                    Else
+                        boxUsername.Text = ""
+                        passwordBox.Text = ""
+                        MsgBox("username atau password salah !")
+                    End If
+                Else
+                    boxUsername.Text = ""
+                    passwordBox.Text = ""
+                    MsgBox("username atau password salah !")
+                End If
+
+
+            Catch ex As Exception
+                MsgBox(ex.ToString())
+            End Try
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        ' Menghentikan semua proses aplikasi ketika aplikasi ditutup
+        Dim currentProcess As Process = Process.GetCurrentProcess()
+        Dim processName As String = currentProcess.ProcessName
+
+        ' Mencari proses dengan nama aplikasi saat ini
+        Dim processes As Process() = Process.GetProcessesByName(processName)
+
+        ' Menghentikan setiap proses dengan nama aplikasi saat ini
+        For Each process As Process In processes
+            If process.Id <> currentProcess.Id Then
+                ' Menghentikan proses lain selain proses utama
+                process.Kill()
+            End If
+        Next
+    End Sub
+End Class
---
+++ Form2.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        PanelSideBar = New Panel()
+        BtnPegawai = New Button()
+        BtnLogout = New Button()
+        BtnKeuangan = New Button()
+        BtnKamar = New Button()
+        BtnTamu = New Button()
+        BtnReservasi = New Button()
+        BtnDashboard = New Button()
+        PanelSideBar.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' PanelSideBar
+        ' 
+        PanelSideBar.BackColor = Color.WhiteSmoke
+        PanelSideBar.Controls.Add(BtnPegawai)
+        PanelSideBar.Controls.Add(BtnLogout)
+        PanelSideBar.Controls.Add(BtnKeuangan)
+        PanelSideBar.Controls.Add(BtnKamar)
+        PanelSideBar.Controls.Add(BtnTamu)
+        PanelSideBar.Controls.Add(BtnReservasi)
+        PanelSideBar.Controls.Add(BtnDashboard)
+        PanelSideBar.Location = New Point(0, 3)
+        PanelSideBar.Name = "PanelSideBar"
+        PanelSideBar.Size = New Size(200, 469)
+        PanelSideBar.TabIndex = 0
+        ' 
+        ' BtnPegawai
+        ' 
+        BtnPegawai.BackColor = Color.MediumOrchid
+        BtnPegawai.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        BtnPegawai.ForeColor = Color.GhostWhite
+        BtnPegawai.Location = New Point(-3, 279)
+        BtnPegawai.Name = "BtnPegawai"
+        BtnPegawai.Size = New Size(200, 36)
+        BtnPegawai.TabIndex = 6
+        BtnPegawai.Text = "Pegawai"
+        BtnPegawai.UseVisualStyleBackColor = False
+        ' 
+        ' BtnLogout
+        ' 
+        BtnLogout.BackColor = Color.MediumOrchid
+        BtnLogout.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        BtnLogout.ForeColor = Color.GhostWhite
+        BtnLogout.Location = New Point(-3, 321)
+        BtnLogout.Name = "BtnLogout"
+        BtnLogout.Size = New Size(200, 36)
+        BtnLogout.TabIndex = 5
+        BtnLogout.Text = "Log Out"
+        BtnLogout.UseVisualStyleBackColor = False
+        ' 
+        ' BtnKeuangan
+        ' 
+        BtnKeuangan.BackColor = Color.MediumOrchid
+        BtnKeuangan.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        BtnKeuangan.ForeColor = Color.GhostWhite
+        BtnKeuangan.Location = New Point(0, 237)
+        BtnKeuangan.Name = "BtnKeuangan"
+        BtnKeuangan.Size = New Size(200, 36)
+        BtnKeuangan.TabIndex = 4
+        BtnKeuangan.Text = "Keuangan"
+        BtnKeuangan.UseVisualStyleBackColor = False
+        ' 
+        ' BtnKamar
+        ' 
+        BtnKamar.BackColor = Color.MediumOrchid
+        BtnKamar.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        BtnKamar.ForeColor = Color.GhostWhite
+        BtnKamar.Location = New Point(0, 195)
+        BtnKamar.Name = "BtnKamar"
+        BtnKamar.Size = New Size(200, 36)
+        BtnKamar.TabIndex = 3
+        BtnKamar.Text = "Kamar"
+        BtnKamar.UseVisualStyleBackColor = False
+        ' 
+        ' BtnTamu
+        ' 
+        BtnTamu.BackColor = Color.MediumOrchid
+        BtnTamu.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        BtnTamu.ForeColor = Color.GhostWhite
+        BtnTamu.Location = New Point(0, 153)
+        BtnTamu.Name = "BtnTamu"
+        BtnTamu.Size = New Size(200, 36)
+        BtnTamu.TabIndex = 2
+        BtnTamu.Text = "Daftar Tamu"
+        BtnTamu.UseVisualStyleBackColor = False
+        ' 
+        ' BtnReservasi
+        ' 
+        BtnReservasi.BackColor = Color.MediumOrchid
+        BtnReservasi.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        BtnReservasi.ForeColor = Color.GhostWhite
+        BtnReservasi.Location = New Point(0, 115)
+        BtnReservasi.Name = "BtnReservasi"
+        BtnReservasi.Size = New Size(200, 36)
+        BtnReservasi.TabIndex = 1
+        BtnReservasi.Text = "Reservasi"
+        BtnReservasi.UseVisualStyleBackColor = False
+        ' 
+        ' BtnDashboard
+        ' 
+        BtnDashboard.BackColor = Color.MediumOrchid
+        BtnDashboard.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        BtnDashboard.ForeColor = Color.GhostWhite
+        BtnDashboard.Location = New Point(0, 73)
+        BtnDashboard.Name = "BtnDashboard"
+        BtnDashboard.Size = New Size(200, 36)
+        BtnDashboard.TabIndex = 0
+        BtnDashboard.Text = "Dashboard"
+        BtnDashboard.UseVisualStyleBackColor = False
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.MediumOrchid
+        ClientSize = New Size(800, 450)
+        Controls.Add(PanelSideBar)
+        ForeColor = Color.MediumOrchid
+        Name = "Form2"
+        Text = "Dashboard - Aplikasi Hotel Kelompok 7"
+        WindowState = FormWindowState.Maximized
+        PanelSideBar.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PanelSideBar As Panel
+    Friend WithEvents BtnDashboard As Button
+    Friend WithEvents BtnReservasi As Button
+    Friend WithEvents BtnTamu As Button
+    Friend WithEvents BtnKeuangan As Button
+    Friend WithEvents BtnKamar As Button
+    Friend WithEvents BtnLogout As Button
+    Friend WithEvents BtnPegawai As Button
+End Class
